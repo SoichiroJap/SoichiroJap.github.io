049b041ad5c80ac216a3aee33d0f2e9b6fc8bfd0
SoichiroJap <turczak123@gmail.com>
2022-05-22 18:49:20 +0200

    stadiony

---
+++ skrypty/stadiony.js
@@ -0,0 +1,48 @@
+function ahmed() {
+    document.getElementById("ahmed").src = "../grafika/ahmed_bin2.jpg";
+}
+function ahmed2() {
+    document.getElementById("ahmed").src = "../grafika/ahmed_bin.jpg";
+}
+function bayt() {
+    document.getElementById("bayt").src = "../grafika/al_bayt2.jpg";
+}
+function bayt2() {
+    document.getElementById("bayt").src = "../grafika/al_bayt.jpg";
+}
+function janoub() {
+    document.getElementById("janoub").src = "../grafika/al_janoub2.jpg";
+}
+function janoub2() {
+    document.getElementById("janoub").src = "../grafika/al_janoub.jpg";
+}
+function thumama() {
+    document.getElementById("thumama").src = "../grafika/al_thumama2.jpg";
+}
+function thumama2() {
+    document.getElementById("thumama").src = "../grafika/al_thumama.jpg";
+}
+function educ() {
+    document.getElementById("educ").src = "../grafika/education_city2.jpg";
+}
+function educ2() {
+    document.getElementById("educ").src = "../grafika/education_city.jpg";
+}
+function khalifa() {
+    document.getElementById("khalifa").src = "../grafika/khalifa2.jpg";
+}
+function khalifa2() {
+    document.getElementById("khalifa").src = "../grafika/khalifa.jpg";
+}
+function lusail() {
+    document.getElementById("lusail").src = "../grafika/lusail2.jpg";
+}
+function lusail2() {
+    document.getElementById("lusail").src = "../grafika/lusail.jpg";
+}
+function ras() {
+    document.getElementById("ras").src = "../grafika/ras_abu2.jpg";
+}
+function ras2() {
+    document.getElementById("ras").src = "../grafika/ras_abu.jpg";
+}
